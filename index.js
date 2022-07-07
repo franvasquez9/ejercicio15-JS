dc4673318aab23f19bd6a6a4dd89a5d96a29944b
franvasquez9 <89528074+franvasquez9@users.noreply.github.com>
2022-07-07 09:29:40 -0500

    ejercicio 15 JS

---
+++ index.js
@@ -0,0 +1,17 @@
+const nombre = "francisco";
+const apellidos = "Vasquez";
+
+const persona = {   
+    nombre: "juan", 
+    apellido: "lopez"
+}
+
+//localStorage.setItem("persona", JSON.stringify({nombre: "juan", apellido: "lopez"}));
+//console.log(localStorage.getItem("persona"));
+
+//sessionStorage.setItem("persona", JSON.stringify({nombre: "juan", apellido: "lopez"}));
+//console.log(sessionStorage.getItem("persona"));
+
+//document.cookie = "nombrecookie=franciscocookie";
+//document.cookie = "caducidad=nombre; expires=" + new Date( 2022, 8, 8).toUTCString()
+//console.log(document.cookie);
